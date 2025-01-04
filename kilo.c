4154db7ea98c1013a3bd114672ffd451ca4c373a
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2025-01-04 20:45:56 +0900

    add: enter key

--- kilo.c
+++ kilo.c
@@ -67,6 +67,7 @@ struct editorConfig E;
 /*** MARK: prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen(void);
 
 /*** MARK: terminal ***/
 void die(const char *s) {
@@ -217,10 +218,12 @@ void editorUpdateRow(erow *row) {
   row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -278,10 +281,25 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline(void) {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar() {
@@ -334,7 +352,7 @@ void editorOpen(char *filename) {
     while (linelen > 0 &&
            (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
       linelen--;
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -503,6 +521,22 @@ void editorSetStatusMessage(const char *fmt, ...) {
 }
 
 /*** MARK: input ***/
+
+// char *editorPrompt(char *prompt) {
+//   size_t bufsize = 128;
+//   char *buf = malloc(bufsize);
+
+//   size_t buflen = 0;
+//   buf[0] = '\0';
+
+//   while (1) {
+//     editorSetStatusMessage(prompt, buf);
+//     editorRefreshScreen();
+
+//     int c = editorReadKey();
+//   }
+// }
+
 void editorMoveCursor(int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
@@ -549,7 +583,7 @@ void editorProcessKeypress(void) {
 
   switch (c) {
     case '\r':
-      /* TODO */
+      editorInsertNewline();
       break;
 
     case CTRL_KEY('q'):
