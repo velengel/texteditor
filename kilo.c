ffb8b1eeb42224c06203bf33c356f8e67424b6cb
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2025-01-13 15:07:56 +0900

    add: colorful serach result

--- kilo.c
+++ kilo.c
@@ -37,7 +37,7 @@ enum editorKey {
   PAGE_DOWN
 };
 
-enum editorHighlight { HL_NORMAL = 0, HL_NUMBER };
+enum editorHighlight { HL_NORMAL = 0, HL_NUMBER, HL_MATCH };
 
 /*** MARK: data ***/
 
@@ -206,6 +206,8 @@ int editorSyntaxToColor(int hl) {
   switch (hl) {
     case HL_NUMBER:
       return 31;
+    case HL_MATCH:
+      return 34;
     default:
       return 37;
   }
@@ -468,6 +470,8 @@ void editorFindCallBack(char *query, int key) {
       E.cy = current;
       E.cx = editorRowRxToCx(row, match - row->render);
       E.rowoff = E.numrows;
+
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
